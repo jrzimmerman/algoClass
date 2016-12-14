5f1f21164d69369f7057645922950516a953bb9f
Justin Zimmerman <zimmy130@gmail.com>
2016-12-13 20:26:55 -0500

    add mergeSort

--- sorting-algorithms/merge.js
+++ sorting-algorithms/merge.js
@@ -28,3 +28,32 @@ subarrays for regular merge sort: [ [1], [2], [4], [5], [9] ]
 subarrays for natural merge sort: [ [1,2], [4,5], [9] ]
 
 */
+
+const merge = (left, right) => {
+  let result = [];
+  while(left.length && right.length) {
+    if(left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  
+  return result.concat(left, right);
+  
+};
+
+const mergeSort = (arr) => {
+  if(arr.length < 2) {
+    return arr;
+  } 
+  
+  let length = arr.length;
+  let mid = Math.floor(length/2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid, length);
+  
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+mergeSort([3,4,2,2,1]);
