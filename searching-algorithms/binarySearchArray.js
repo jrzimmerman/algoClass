d0db152cd77f95b594c224b5bf6306b3ec8e3171
Justin Zimmerman <zimmy130@gmail.com>
2016-12-30 18:19:55 -0500

    complete binary search array

--- searching-algorithms/binarySearchArray.js
+++ searching-algorithms/binarySearchArray.js
@@ -12,3 +12,19 @@ Write a function that takes a sorted array and a value and returns the index of 
 Extra credit: Implement the function both iteratively and recursively.
 
 */
+
+const binarySearchArray = (arr, target) => {
+  const length = arr.length;
+  if (length < 2) {
+    return arr;
+  }
+  const mid = Math.floor(length / 2);
+  if (arr[mid] === target) {
+    return mid;
+  }
+  if(arr[mid] < target) {
+    return binarySearchArray(arr.slice(0, mid), target);
+  } else {
+    return binarySearchArray(arr.slice(mid,length), target);
+  }
+}
